5c17815ce09b2fcaacc2321ade280e2aaaef0706
AlexandreMarcelino <alexandre.msantos@mercadolivre.com>
2023-01-10 18:16:18 -0300

    Arquivo para envio a Generation criado

---
+++ java/tipos-e-operadores-basicos/src/ExerciciosGerais.java
@@ -0,0 +1,82 @@
+/*
+public class ExerciciosGerais {
+
+    public class Exercicio01 {
+        public static void main(String[] args) {
+            //Dado o Fluxograma abaixo, desenvolva o algoritmo
+            Scanner sc = new Scanner(System.in);
+
+            float salariof, abonof;
+
+            System.out.print("Digite o salário: ");
+            salariof = sc.nextFloat();
+            System.out.print("Digite o abono: ");
+            abonof = sc.nextFloat();
+
+            System.out.printf("Novo Salário: %.2f", salariof + abonof);
+        }
+    }
+
+    //
+    public class Exercicio02 {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+
+            float nota1f, nota2f, nota3f, nota4f, media;
+
+            System.out.print("Nota 1: ");
+            nota1f = sc.nextFloat();
+            System.out.print("Nota 2: ");
+            nota2f = sc.nextFloat();
+            System.out.print("Nota 3: ");
+            nota3f = sc.nextFloat();
+            System.out.print("Nota 4: ");
+            nota4f = sc.nextFloat();
+
+            media = (nota1f + nota2f + nota3f + nota4f) / 4;
+
+            System.out.printf("Média final: %.1f ", media);
+        }
+    }
+
+    //
+    public class Exercicio03 {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+
+            float salBruto, adNoturno, horasEx, descontos;
+
+            System.out.print("Digite o salário: ");
+            salBruto = sc.nextFloat();
+            System.out.print("Informe o adicional noturno: ");
+            adNoturno = sc.nextFloat();
+            System.out.print("Informe a quantidade de horas extras: ");
+            horasEx = sc.nextFloat();
+            System.out.print("Informe o valor dos descontos: ");
+            descontos = sc.nextFloat();
+
+            System.out.printf("Salário líquido: %.2f", salBruto + adNoturno + (horasEx * 5) - descontos);
+        }
+    }
+
+    //
+    public class Exercicio04 {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+
+            int num1, num2, num3, num4;
+
+            System.out.print("Numero 1: ");
+            num1 = sc.nextInt();
+            System.out.print("Numero 2: ");
+            num2 = sc.nextInt();
+            System.out.print("Numero 3: ");
+            num3 = sc.nextInt();
+            System.out.print("Numero 4: ");
+            num4 = sc.nextInt();
+
+            System.out.print("Diferença: " + ((num1 * num2) - (num3 * num4)));
+        }
+    }
+}
+*/
