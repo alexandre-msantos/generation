02ffe135e02e211998ec860b1ac0f7b6ddfbda22
AlexandreMarcelino <alexandre.msantos@mercadolivre.com>
2023-01-10 12:36:56 -0300

    modificacao nas variaveis

--- java/tipos-e-operadores-basicos/src/Exercicio01.java
+++ java/tipos-e-operadores-basicos/src/Exercicio01.java
@@ -3,14 +3,15 @@ import java.util.Scanner;
 public class Exercicio01 {
     public static void main(String[] args) {
         //Dado o Fluxograma abaixo, desenvolva o algoritmo
-
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Digite o salário: ");
-        float salario = sc.nextFloat();
-        System.out.print("Digite o abono: ");
-        float abono = sc.nextFloat();
+        float salariof, abonof;
 
-        System.out.printf("Novo Salário: %.2f", salario + abono);
+        System.out.print("Digite o salário: ");
+        salariof = sc.nextFloat();
+        System.out.print("Digite o abono: ");
+        abonof = sc.nextFloat();
+
+        System.out.printf("Novo Salário: %.2f", salariof + abonof);
     }
 }
